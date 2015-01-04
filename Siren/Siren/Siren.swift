f4c539f82f963fbfa07f7d4439f52eb32181fd42
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-01-03 20:00:37 -0500

    Added Hebrew and Turkish cases to SirenLanguage enum

--- Siren/Siren/Siren.swift
+++ Siren/Siren/Siren.swift
@@ -10,9 +10,10 @@ import Foundation
 import UIKit
 
 /**
-Localization constants
+    Localization constants
 */
-public enum SirenLanguage: String {
+public enum SirenLanguage: String
+{
     case Basque = "eu"
     case ChineseSimplified = "zh-Hans"
     case ChineseTraditional = "zh-Hant"
@@ -20,6 +21,7 @@ public enum SirenLanguage: String {
     case Dutch = "nl"
     case English = "en"
     case French = "fr"
+    case Hebrew = "he"
     case German = "de"
     case Italian = "it"
     case Japanese = "ja"
@@ -29,9 +31,10 @@ public enum SirenLanguage: String {
     case Slovenian = "sl"
     case Sweidsh = "sv"
     case Spanish = "es"
+    case Turkish = "tr"
 }
 
-class Siren : NSObject
+public class Siren : NSObject
 {
     
 }

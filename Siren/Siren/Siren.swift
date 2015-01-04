7347b93fe97835f762e87006490a3cf5dd8a8272
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-01-03 20:27:52 -0500

    Post-merge consolidation

--- Siren/Siren/Siren.swift
+++ Siren/Siren/Siren.swift
@@ -9,7 +9,15 @@
 import Foundation
 import UIKit
 
-// MARK: Constants
+// MARK: Delegate
+@objc protocol SirenDelegate {
+    optional func sirenDidShowUpdateDialog()       // User presented with update dialog
+    optional func sirenUserDidLaunchAppStore()     // User did click on button that launched App Store.app
+    optional func sirenUserDidSkipVersion()        // User did click on button that skips version update
+    optional func sirenUserDidCancel()             // User did click on button that cancels update dialog
+}
+
+// MARK: Enumerations
 /**
     Localization constants
 */
@@ -35,6 +43,14 @@ public enum SirenLanguage: String
     case Turkish = "tr"
 }
 
+public enum SirenAlertType
+{
+    case Force        // Forces user to update your app
+    case Option       // (DEFAULT) Presents user with option to update app now or at next launch
+    case Skip         // Presents User with option to update the app now, at next launch, or to skip this version all together
+    case None         // Don't show the alert type , usefull for skipping Patch ,Minor, Major update
+}
+
 // MARK: Main Class
 public class Siren : NSObject
 {
@@ -46,7 +62,54 @@ public class Siren : NSObject
         return Singleton.instance
     }
     
-    public override func init() {
+    // From Aaron's branch
+    weak var delegate : HarpyDelegate?
+    var presentingViewController : UIViewController?
+    
+    var appID : String?
+    var appName : String = (NSBundle.mainBundle().infoDictionary?[kCFBundleNameKey] as? String) ?? ""
+    
+    var countryCode : String?
+    var forceLanguageLocalization : SirenLanguage?
+    
+    var debugEnabled = false
+    
+    var alertType = SirenAlertType.Option
+    var majorUpdateAlertType = SirenAlertType.Option
+    var minorUpdateAlertType = SirenAlertType.Option
+    var patchUpdateAlertType = SirenAlertType.Option
+    
+    var alertControllerTintColor : UIColor?
+    
+    let HARPY_DEFAULT_SHOULD_SKIP_VERSION = "Harpy Should Skip Version Boolean"
+    let HARPY_DEFAULT_SKIPPED_VERSION = "Harpy User Decided To Skip Version Update Boolean"
+    let HARPY_DEFAULT_STORED_VERSION_CHECK_DATE = "Harpy Stored Date From Last Version Check"
+    
+    let currentVersion = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String?
+    let bundlePath = NSBundle.mainBundle().pathForResource("Harpy", ofType: "Bundle")
+    
+    class func iTunesURLString(id : String, country : String?) -> String {
+        var url = "http://itunes.apple.com/lookup?id=\(id)"
+        
+        if let country = country {
+            url += "&country=\(country)"
+        }
+        
+        return url
+    }
+    
+    func sirenBundle -> NSBundle? {
+        var bundle : NSBundle
+        var path : String?
+        
+        if let language = forceLanguageLocalization {
+            path = NSBundle(path: bundlePath!)?.pathForResource(language, ofType: "lproj")
+        } else {
+            
+        }
+    }
+    
+    func harpyLocalizedString(key : String) -> String {
         
     }
     

337c97e0b24c85cb242b90810c836950a2f45ed6
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-01-03 19:46:05 -0500

    Initial code and asset commit

---
+++ Siren/Siren/Siren.swift
@@ -0,0 +1,37 @@
+//
+//  Siren.swift
+//  Siren
+//
+//  Created by Arthur Sabintsev on 1/3/15.
+//  Copyright (c) 2015 Sabintsev iOS Projects. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/**
+Localization constants
+*/
+public enum SirenLanguage: String {
+    case Basque = "eu"
+    case ChineseSimplified = "zh-Hans"
+    case ChineseTraditional = "zh-Hant"
+    case Danish = "da"
+    case Dutch = "nl"
+    case English = "en"
+    case French = "fr"
+    case German = "de"
+    case Italian = "it"
+    case Japanese = "ja"
+    case Korean = "ko"
+    case Portuguese = "pt"
+    case Russian = "ru"
+    case Slovenian = "sl"
+    case Sweidsh = "sv"
+    case Spanish = "es"
+}
+
+class Siren : NSObject
+{
+    
+}
---
+++ Siren/Siren/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Siren
+//
+//  Created by Arthur Sabintsev on 1/3/15.
+//  Copyright (c) 2015 Sabintsev iOS Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

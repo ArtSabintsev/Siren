b6cd1f6027f723cccc6ad15b96564e240bee7d17
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2016-01-07 18:58:56 -0600

    Improved inline documentation

--- Siren/Siren.swift
+++ Siren/Siren.swift
@@ -22,10 +22,11 @@ import UIKit
     Determines the type of alert to present after a successful version check has been performed.
     
     There are four options:
-        - Force: Forces user to update your app (1 button alert)
-        - Option: (DEFAULT) Presents user with option to update app now or at next launch (2 button alert)
-        - Skip: Presents user with option to update the app now, at next launch, or to skip this version all together (3 button alert)
-        - None: Doesn't show the alert, but instead returns a localized message for use in a custom UI within the sirenDidDetectNewVersionWithoutAlert() delegate method
+
+    - .Force: Forces user to update your app (1 button alert)
+    - .Option: (DEFAULT) Presents user with option to update app now or at next launch (2 button alert)
+    - .Skip: Presents user with option to update the app now, at next launch, or to skip this version all together (3 button alert)
+    - .None: Doesn't show the alert, but instead returns a localized message for use in a custom UI within the sirenDidDetectNewVersionWithoutAlert() delegate method
 
 */
 public enum SirenAlertType {

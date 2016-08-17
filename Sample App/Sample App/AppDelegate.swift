644626a8dee7c2b763b62e4f9dffc56497cf9326
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2016-08-16 21:14:10 -0400

    Fixed sample project

--- Sample App/Sample App/AppDelegate.swift
+++ Sample App/Sample App/AppDelegate.swift
@@ -14,10 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+
         window?.makeKeyAndVisible()
-        
+
         setupSiren()
 
         return true

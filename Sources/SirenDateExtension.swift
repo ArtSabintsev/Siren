379d4b3d8251df286605c2375bc178b1f04aadb4
Ryoh Tsukahara <ryoh.t778@gmail.com>
2017-06-20 02:57:59 +0900

    Set "en_US_POSIX" as the locale of the date formatter (#150)

--- Sources/SirenDateExtension.swift
+++ Sources/SirenDateExtension.swift
@@ -17,6 +17,7 @@ internal extension Date {
 
     static func days(since dateString: String) -> Int? {
         let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "en_US_POSIX")
         dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
         guard let releaseDate = dateformatter.date(from: dateString) else {

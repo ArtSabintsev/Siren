2647eb6f836d5cc162948b31a10e79928a33dacd
nixnoughtnothing <ryoh.t778@gmail.com>
2017-08-27 20:04:14 +0900

    Updated test for Japanese localization

--- SirenExample/SirenExampleTests/SirenTests.swift
+++ SirenExample/SirenExampleTests/SirenTests.swift
@@ -449,7 +449,7 @@ extension SirenTests {
         siren.forceLanguageLocalization = language
 
         // Update Available
-        XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Update Available", forceLanguageLocalization: language), "更新が利用可能")
+        XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Update Available", forceLanguageLocalization: language), "アップデートのお知らせ")
 
         // Next time
         XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Next time", forceLanguageLocalization: language), "次回")
@@ -458,7 +458,7 @@ extension SirenTests {
         XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Skip this version", forceLanguageLocalization: language), "このバージョンをスキップ")
 
         // Update
-        XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Update", forceLanguageLocalization: language), "更新")
+        XCTAssertEqual(Bundle().testLocalizedString(stringKey: "Update", forceLanguageLocalization: language), "アップデート")
     }
 
     func testKoreanLocalization() {

10f8f610e84fe1d0f9640ecc190f9313a5249026
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2018-12-03 22:35:25 -0500

    Removed redundant comments

--- Sources/Models/Localization.swift
+++ Sources/Models/Localization.swift
@@ -17,89 +17,47 @@ public struct Localization {
     /// By default, the operating system's default lanuage setting is used. However, you can force a specific language
     /// by setting the forceLanguageLocalization property before calling checkVersion()
     public enum Language: String {
-        /// Arabic
         case arabic = "ar"
-        /// Armenian
         case armenian = "hy"
-        /// Basque
         case basque = "eu"
-        /// Simplified Chinese
         case chineseSimplified = "zh-Hans"
-        /// Traditional Chinese
         case chineseTraditional = "zh-Hant"
-        /// Croatian
         case croatian = "hr"
-        /// Czech
         case czech = "cs"
-        /// Danish
         case danish = "da"
-        /// Dutch
         case dutch = "nl"
-        /// English
         case english = "en"
-        /// Estonian
         case estonian = "et"
-        /// Finnish
         case finnish = "fi"
-        /// French
         case french = "fr"
-        /// German
         case german = "de"
-        /// Greek
         case greek = "el"
-        /// Hebrew
         case hebrew = "he"
-        /// Hungarian
         case hungarian = "hu"
-        /// Indonesian
         case indonesian = "id"
-        /// Italian
         case italian = "it"
-        /// Japanese
         case japanese = "ja"
-        /// Korean
         case korean = "ko"
-        /// Latvian
         case latvian = "lv"
-        /// Lithuanian
         case lithuanian = "lt"
-        /// Malaysian
         case malay = "ms"
-        /// Norwegian
         case norwegian = "nb-NO"
-        /// Persian
         case persian = "fa"
-        /// Persian (Afghanistan)
         case persianAfghanistan = "fa-AF"
-        /// Persian (Iran)
         case persianIran = "fa-IR"
-        /// Polish
         case polish = "pl"
-        /// Portuguese (Brazil)
         case portugueseBrazil = "pt"
-        /// Portuguese (Portugal)
         case portuguesePortugal = "pt-PT"
-        /// Russian
         case russian = "ru"
-        /// Serbian (Cyrillic)
         case serbianCyrillic = "sr-Cyrl"
-        /// Serbian (Latin)
         case serbianLatin = "sr-Latn"
-        /// Slovenian
         case slovenian = "sl"
-        /// Spanish
         case spanish = "es"
-        /// Swedish
         case swedish = "sv"
-        /// Thai
         case thai = "th"
-        /// Turkish
         case turkish = "tr"
-        /// Urdu
         case urdu = "ur"
-        /// Ukranian
         case ukrainian = "uk"
-        /// Vietnamese
         case vietnamese = "vi"
     }
 

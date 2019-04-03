a4201197655c019b028607d54e86ef85ef548ab4
Cihat Gündüz <github@cihatguenduez.de>
2019-04-03 05:29:59 +0200

    Update SwiftPM manifest file (#281)
    * Update SwiftPM manifest file
    
    * [README] Add 'Accio supported' badge

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-  name: "Siren"
+    name: "Siren",
+    // platforms: [.iOS("8.0")],
+    products: [
+        .library(name: "Siren", targets: ["Siren"])
+    ],
+    targets: [
+        .target(
+            name: "Siren",
+            path: "Sources"
+        )
+    ]
 )

c9fb91a3ed3d746d73dc780566dc1d69206b30da
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2019-04-16 21:20:30 -0400

    Update DateExtension to remove whitespaces

--- Sources/Extensions/DateExtension.swift
+++ Sources/Extensions/DateExtension.swift
@@ -30,11 +30,11 @@ extension Date {
         dateformatter.locale = Locale(identifier: "en_US_POSIX")
         dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
         dateformatter.timeZone = TimeZone(secondsFromGMT: 0)
-        
+
         guard let date = dateformatter.date(from: dateString) else {
             return nil
         }
-        
+
         return days(since: date)
     }
 }

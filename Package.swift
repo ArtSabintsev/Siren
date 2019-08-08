47b0b5381e48b7192e053ae0e259a6f77ee384db
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2019-08-07 21:20:29 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,8 @@
-// swift-tools-version:4.2
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Siren",
-    // platforms: [.iOS("8.0")],
-    products: [
-        .library(name: "Siren", targets: ["Siren"])
-    ],
-    targets: [
-        .target(
-            name: "Siren",
-            path: "Sources"
-        )
-    ]
+    products: [.library(name: "Siren", targets: ["Siren"])],
+    targets: [.target(name: "Siren", path: "Sources")]
 )

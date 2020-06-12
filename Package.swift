c3eb837d8f265c9db6f97a1d633b0b9e48513cbc
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-12 12:20:36 -0400

    Updated manifest...again

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "Siren",
-    swiftLanguageVersions: [.v5],
     products: [.library(name: "Siren", targets: ["Siren"])],
     targets: [.target(name: "Siren", path: "Sources")]
 )

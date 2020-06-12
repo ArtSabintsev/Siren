84ece2335fd5a8331776d9572e136b842305ff2c
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-12 10:50:23 -0400

    e-updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Siren",
     swiftLanguageVersions: [.v5],
-    defaultLocalization: LocalizationTag = "en,
+    defaultLocalization: LocalizationTag = "en",
     products: [.library(name: "Siren", targets: ["Siren"])],
     targets: [.target(name: "Siren", path: "Sources")]
 )

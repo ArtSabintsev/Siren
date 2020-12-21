2cffe34e2cdc81a5c5780d7fbf72d30469a37393
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-12-21 08:37:55 -0500

    Fix Swift PM 5.3 Resource Bundle Migration (#358)
    * Updated lockfile
    
    * Updated package.swift
    
    * Updated package.swift
    
    * Updated BundleExtension
    
    * Minified Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     name: "Siren",
     platforms: [.iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "Siren", targets: ["Siren"])],
-    targets: [.target(name: "Siren", path: "Sources")],
+    targets: [.target(name: "Siren", path: "Sources", resources: [.copy("Siren.bundle")])],
     swiftLanguageVersions: [.v5]
 )

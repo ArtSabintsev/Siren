c13b97311900f15789a0045f67e23fe62c7e7f43
Arthur Sabintsev <arthur@sabintsev.com>
2020-12-20 23:45:40 -0500

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "Siren",
-    platforms: [.iOS(.v11), .tvOS(.v11)],
-    products: [.library(name: "Siren", targets: ["Siren"])],
-    targets: [.target(name: "Siren", path: "Sources")],
+    platforms: [
+        .iOS(.v11), 
+        .tvOS(.v11)
+    ],
+    products: [
+        .library(name: "Siren", targets: ["Siren"])
+    ],
+    targets: [
+        .target(name: "Siren", path: "Sources", resources: [.process("Siren.bundle")])
+    ],
     swiftLanguageVersions: [.v5]
 )

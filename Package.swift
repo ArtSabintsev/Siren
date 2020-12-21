592e44fbb69ee69d93acceb6e023ce58d66f61f5
Arthur Sabintsev <arthur@sabintsev.com>
2020-12-21 08:37:26 -0500

    Minified Package.swift file

--- Package.swift
+++ Package.swift
@@ -3,18 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "Siren",
-    platforms: [
-        .iOS(.v11), 
-        .tvOS(.v11)
-    ],
-    products: [
-        .library(name: "Siren", targets: ["Siren"])
-    ],
-    targets: [
-        .target(
-            name: "Siren", 
-            path: "Sources", 
-            resources: [.copy("Siren.bundle")])
-    ],
+    platforms: [.iOS(.v11), .tvOS(.v11)],
+    products: [.library(name: "Siren", targets: ["Siren"])],
+    targets: [.target(name: "Siren", path: "Sources", resources: [.copy("Siren.bundle")])],
     swiftLanguageVersions: [.v5]
 )

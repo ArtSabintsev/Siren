4ccd61d1c5cec1ffe2869ce79689d0969000ad42
Marek Zvara <marek724@gmail.com>
2024-04-17 12:02:35 +0200

    Format package file

--- Package.swift
+++ Package.swift
@@ -4,7 +4,18 @@ import PackageDescription
 let package = Package(
     name: "Siren",
     platforms: [.iOS(.v13), .tvOS(.v13)],
-    products: [.library(name: "Siren", targets: ["Siren"])],
-    targets: [.target(name: "Siren", path: "Sources", resources: [.copy("Siren.bundle")])],
+    products: [
+        .library(
+            name: "Siren",
+            targets: ["Siren"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Siren",
+            path: "Sources",
+            resources: [.copy("Siren.bundle")]
+        )
+    ],
     swiftLanguageVersions: [.v5]
 )

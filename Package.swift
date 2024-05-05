b0347390659df4c192da2e56eea671f27553a442
Steve Spigarelli <spigarellisj@churchofjesuschrist.org>
2024-05-04 18:21:14 -0600

    Add PrivacyInfo (#422)
    Close #420 in favor of #422.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     name: "Siren",
     platforms: [.iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "Siren", targets: ["Siren"])],
-    targets: [.target(name: "Siren", path: "Sources", resources: [.copy("Siren.bundle")])],
+    targets: [.target(name: "Siren", path: "Sources", resources: [.copy("Siren.bundle"), .copy("PrivacyInfo.xcprivacy")])],
     swiftLanguageVersions: [.v5]
 )
